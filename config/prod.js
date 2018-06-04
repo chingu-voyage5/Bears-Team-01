d3a3fd88546bdfdaa2578ae566d24ce1c851ef49
Eric <zaknefeinn@gmail.com>
2018-06-04 16:12:38 -0500

    auth and server structure

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-  testKey: process.env.TEST_KEY
+  testKey: process.env.TEST_KEY,
+  mongoURI: process.env.MONGO_URI
 };
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
 
 const app = express();
 app.use(morgan('dev'));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send(keys.testKey);
 });
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log('connected'))
+  .catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
