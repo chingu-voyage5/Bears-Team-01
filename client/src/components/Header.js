2000191e29e0537802a8f800bbae35e8399cc4f5
Zaknefeinn <zaknefeinn@gmail.com>
2018-06-07 16:49:00 -0500

    end of work day

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#root');
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+    // this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  // afterOpenModal() {
+  //   // references are now sync'd and can be accessed.
+  //   this.subtitle.style.color = '#f00';
+  // }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
   render() {
     return (
       <nav className="header">
         <h1 className="logo">WhatsNext.Fun</h1>
-        <button>Sign Up </button>
+        <button onClick={this.openModal}>Sign Up </button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <form>
+            <input placeholder="e-mail" />
+            <input placeholder="name" />
+            <input placeholder="password" />
+          </form>
+        </Modal>
       </nav>
     );
   }
