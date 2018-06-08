24835c12c0487f289911b998019ccff9df962720
Jai <contactjaitiwari@gmail.com>
2018-06-08 12:51:11 -0400

    added route to server static files for production env

--- server.js
+++ server.js
@@ -29,5 +29,13 @@ require('./config/passport')(passport);
 
 app.use('/api/users', users);
 
+if (process.env.NODE_END === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
